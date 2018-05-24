99c41973d0628e8ba2fea10584fb7d947e82d9e8
rileymburton <35310092+rileymburton@users.noreply.github.com>
2018-05-23 19:51:27 -0700

    phone number checker

---
+++ main.py
@@ -0,0 +1,25 @@
+def isPhoneNumber(text):
+    if len(text) != 12:
+        return False
+    for i in range(0, 3):
+        if not text[i].isdecimal():
+            return False
+    if text[3] != '-':
+        return False
+    for i in range(4, 7):
+        if not text[i].isdecimal():
+            return False
+    if text[7] != '-':
+        return False
+    for i in range(8, 12):
+        if not text[i].isdecimal():
+            return False
+    return True
+
+message = 'Call me at 503-484-4699 tomorrow. 971-331-9903 is my work number.'
+for i in range(len(message)):
+    chunk = message[i:i+12]
+    if isPhoneNumber(chunk):
+        print('Phone number found: {}'.format(chunk))
+print('Done')
+print("yah")
