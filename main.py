179d7d4a4bef39523d15155e3e9ab556ab3705c7
rileymburton <35310092+rileymburton@users.noreply.github.com>
2018-05-23 20:44:54 -0700

    phone number finder

--- main.py
+++ main.py
@@ -1,25 +1,5 @@
-def isPhoneNumber(text):
-    if len(text) != 12:
-        return False
-    for i in range(0, 3):
-        if not text[i].isdecimal():
-            return False
-    if text[3] != '-':
-        return False
-    for i in range(4, 7):
-        if not text[i].isdecimal():
-            return False
-    if text[7] != '-':
-        return False
-    for i in range(8, 12):
-        if not text[i].isdecimal():
-            return False
-    return True
+import re
 
-message = 'Call me at 503-484-4699 tomorrow. 971-331-9903 is my work number.'
-for i in range(len(message)):
-    chunk = message[i:i+12]
-    if isPhoneNumber(chunk):
-        print('Phone number found: {}'.format(chunk))
-print('Done')
-print("yah")
+phoneNumberRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+mo1 = phoneNumberRegex.search('My number is 503-484-4699')
+print(mo1.group())
